c57acf2ada8a1773aed60c2e0b8b3b20cb37a1cc
shehrozeikram <shehrozeikram2@gmail.com>
2024-02-13 13:07:48 +0500

    front end done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,29 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def booking
+  end
+
+  def contact
+  end
+
+  def destination
+  end
+
+  def service
+  end
+
+  def team
+  end
+
+  def testimonial
+  end
+
+  def about
+  end
+
+  def package
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
 
   root "home#index"
-  get 'home/index'
+
+  get 'home/index', to: 'home#index'
+  get 'home/booking', to: 'home#booking'
+  get 'home/contact', to: 'home#contact'
+  get 'home/destination', to: 'home#destination'
+  get 'home/service', to: 'home#service'
+  get 'home/team', to: 'home#team'
+  get 'home/testimonial', to: 'home#testimonial'
+  get 'home/about', to: 'home#about'
+  get 'home/package', to: 'home#package'
 
 
 
